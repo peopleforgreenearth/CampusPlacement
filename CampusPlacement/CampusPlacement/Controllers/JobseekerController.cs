8a51245fe6e0453719bb4444d243dcf78cb0dac8
peopleforgreenearth <harpreet.ind@gmail.com>
2014-10-18 07:08:55 +0530

    schema changes

---
+++ CampusPlacement/CampusPlacement/Controllers/JobseekerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CampusPlacement.Controllers
+{
+    public class JobseekerController : Controller
+    {
+        //
+        // GET: /Jobseeker/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ CampusPlacement/CampusPlacement/ViewModels/SkillSearchModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CampusPlacement.ViewModels
+{
+    public class SkillSearchModel
+    {
+        public string Skills { get; set; }
+        public List<Models.Country> Country { get; set; }
+        public List<Models.State> State { get; set; }
+    }
+}
